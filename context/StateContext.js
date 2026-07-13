54b6d5165f4722949464ae2b2f221215d1919756
CondoRHxH <fahdcondor04@gmail.com>
2026-07-13 18:43:10 +0100

    CartFinished

--- context/StateContext.js
+++ context/StateContext.js
@@ -68,7 +68,7 @@ export const StateContext = ({children}) => {
 
     const onRemove = (product) =>{
         foundProduct = cartItems.find((item) => item._id == product._id)
-        const newCartItems = cartItems.filter((item) => item._id !== id)
+        const newCartItems = cartItems.filter((item) => item._id !== product._id)
         
         setTotalPrice((prevtotalPrice) => prevtotalPrice - foundProduct.price * foundProduct.quantity)
         setTotalQuantities((prevtotalQuantities) => prevtotalQuantities - foundProduct.quantity)
