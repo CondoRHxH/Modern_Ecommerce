a06750b5cfedd885761f41f65f5a34b8a690eb47
CondoRHxH <fahdcondor04@gmail.com>
2026-07-15 18:26:54 +0100

    Stripe Integration

--- api/Stripe.js
+++ api/Stripe.js
@@ -1,14 +1,31 @@
+import { headers } from 'next/headers';
 import 'server-only'
 import Stripe from 'stripe'
 
 const Stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
-export default async function handler(req, res){
-    if(req.method === 'POST'){
-        try{
+export async function POST(){
+    try{
+        const headersList = await headers()
+        const origin = headersList.get('origin')
 
-        } catch(error){
-            res.status(500).json({statusCode : 500, message : error.message})
-        }
+        const session = await stripe.checkout.session.create({
+            line_items: [
+                {
+                    price:'foundProduct.price',
+                    quantity : 1,
+                },
+            ],
+            mode: 'payment',
+            success_url : `${origin}/success?session_id={}`,
+            cancel_url: `${origin}/?canceled=true`,
+    })
+
+    }catch(error){
+        console.error("Stripe error",error);
+        return NextResponse.json(
+            { error:error.message },
+            { status:error.statusCode || 500 }
+        )
     }
 }
