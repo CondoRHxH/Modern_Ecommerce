bb3d1d35afec2937478b05110b2740d795e1f781
CondoRHxH <fahdcondor04@gmail.com>
2026-08-16 01:27:52 +0100

    Navbar Pending

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,18 @@
 "use client"
-
+// import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 import React, { useState } from 'react'
 
 import Link from 'next/link'
@@ -11,9 +24,15 @@ import { useStateContext } from '../context/StateContext'
 const Navbar = () =>{
     const {showCart, setShowCart, totalQuantities} = useStateContext()
     
+    const pages = ['Products', 'Pricing', 'Blog'];
+    const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+    
+
     const data = useStateContext()
     console.log(data)
     return(
+        
         <div className='navbar-container'>
             <p><Link href='/'>Store</Link></p>
                 
